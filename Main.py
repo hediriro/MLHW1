bc9658a3e433bc0d76786cc53c224564df108249
Henning <henning.richter@uni-rostock.de>
2020-05-18 16:15:40 +0200

    minor fix

--- Main.py
+++ Main.py
@@ -3,19 +3,17 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-from sklearn import preprocessing
+from sklearn import preprocessing, kernel_approximation, neighbors
 
 df = pd.read_csv('dataset_191_wine.csv')  # create data frame
 X = df.iloc[:, 1:]  # create train frame
 Y = df.iloc[:, 0]  # create target vector
 print('X:', X.shape, ' y:', Y.shape)
 
-X_norm = preprocessing.normalize(X) # normalizing attributes
+X_norm = preprocessing.normalize(X)  # normalizing attributes
 # naive is already given
-Y_ohe = pd.get_dummies(Y) # One Hot Encoding
+Y_ohe = pd.get_dummies(Y)  # One Hot Encoding
 
-
-
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=(1 / len(Y)), random_state=42)  # split sets & (len(Y)-1) -> leave one out
+X_train, X_test, Y_train, Y_test = train_test_split(X_norm, Y, test_size=(1 / len(Y)), random_state=42)  # split sets & leave one out
 print('X_train:', X_train.shape, ' Y_train:', Y_train.shape)
 print('X_test:', X_test.shape, ' Y_test:', Y_test.shape)
