d7fd17ddc20196763a0212737678ab77c9d2b7a6
Henning <henning.richter@uni-rostock.de>
2020-05-18 15:48:24 +0200

    One Hot Encoding & Naive

--- Main.py
+++ Main.py
@@ -3,21 +3,19 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import OneHotEncoder
 
 df = pd.read_csv('dataset_191_wine.csv')  # create data frame
 X = df.iloc[:, 1:]  # create train frame
 Y = df.iloc[:, 0]  # create target vector
-# print('X:', X.shape, ' y:', Y.shape)
+print('X:', X.shape, ' y:', Y.shape)
 
 X = mathhelp.normalize(X)  # normalizing attributes
 
-enc = OneHotEncoder(handle_unknown='ignore')
-enc.fit_transform()
+Y_h = pd.get_dummies(Y) # One Hot Encoding
+print(Y_h.head(3))
 
 
 
-
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=(1 / (len(Y) - 1)), random_state=42)  # split sets & (len(Y)-1) -> leave one out
-# print('X_train:', X_train.shape, ' Y_train:', Y_train.shape)
-# print('X_test:', X_test.shape, ' Y_test:', Y_test.shape)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=(1 / len(Y)), random_state=42)  # split sets & (len(Y)-1) -> leave one out
+print('X_train:', X_train.shape, ' Y_train:', Y_train.shape)
+print('X_test:', X_test.shape, ' Y_test:', Y_test.shape)
--- mathhelp.py
+++ mathhelp.py
@@ -2,9 +2,9 @@ import math
 import numpy as np
 
 def round_as_naive_approach(result): # the naive way
-    if result result < 1.5:
+    if result < 1.5:
         return 1
-    if result >= 1.5 && result < 2.5:
+    if (result >= 1.5) and (result < 2.5):
         return 2
     if result >= 2.5:
         return 3
