a7c7b7d95529f884d55a2834d694f867d8affe64
Henning <henning.richter@uni-rostock.de>
2020-05-18 14:02:45 +0200

    small modify

--- Main.py
+++ Main.py
@@ -4,14 +4,16 @@ import csv
 
 with open('dataset_191_wine.csv', mode='r') as csv_file:
     csv_reader: csv.DictReader = csv.reader(csv_file, quoting=csv.QUOTE_NONNUMERIC) # from string to float
+    column_counter = 0
     for row in csv_reader:
         list_of_rows = list(csv_reader)  # 1st col with col_names not part of
 
 data_set = list_of_rows
-result_set = data_set
 
 for j in data_set: # remove results / 1st column
     del j[0]
 
 for i in range(0, len(data_set)): # normalizing vectors
     data_set[i] = mathhelp.normalize(data_set[i])
+
+
