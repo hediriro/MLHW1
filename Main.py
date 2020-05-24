a07b1a3db276c585397e42be80318b0f2fb7ef02
Henning <henning.richter@uni-rostock.de>
2020-05-25 00:29:25 +0200

    unfinished

--- Main.py
+++ Main.py
@@ -6,6 +6,7 @@ from sklearn.model_selection import train_test_split, GridSearchCV, LeaveOneOut
 from sklearn.preprocessing import normalize
 
 df = pd.read_csv('dataset_191_wine.csv')  # create data frame
+df = df.sample(frac=1).reset_index(drop=True) # shuffle data frame
 X = df.iloc[:, 1:]  # create train frame
 Y = df.iloc[:, 0]  # create target vector
 X_norm = normalize(X)  # normalizing attributes
@@ -45,20 +46,22 @@ print(parameters_knn)
 X_train, X_test, Y_train, Y_test = train_test_split(X_norm, Y, test_size=(1 / len(Y)), random_state=42)  # split sets & leave one out
 print('X_train:', X_train.shape, ' Y_train:', Y_train.shape)
 print('X_test:', X_test.shape, ' Y_test:', Y_test.shape)
+y_bla = on.predict(X_train, X_test, Y_train, 1)
+print("Expected: ", Y_test, " / Received:", y_bla)
 
 
 # One Hot Encoding
 
 # hyperparameter tuning
-gs_knn = GridSearchCV(KNeighborsClassifier(), grid_params_knn, verbose = 1, cv = LeaveOneOut(), n_jobs = -1, scoring ="f1_micro")
-gs_knn_results = gs_knn.fit(X_norm, Y_ohe)
-parameters_knn = gs_knn.best_params_
-print(parameters_knn)
-
 gs_kernel = GridSearchCV(KernelDensity(), grid_params_kernel, cv = LeaveOneOut())
 gs_kernel.fit(X_norm, Y_ohe)
 parameters_kernel = gs_kernel.best_params_
 print(parameters_kernel)
+
+gs_knn = GridSearchCV(KNeighborsClassifier(), grid_params_knn, verbose = 1, cv = LeaveOneOut(), n_jobs = -1, scoring ="f1_micro")
+gs_knn_results = gs_knn.fit(X_norm, Y_ohe)
+parameters_knn = gs_knn.best_params_
+print(parameters_knn)
 
 X_train_ohe, X_test_ohe, Y_train_ohe, Y_test_ohe = train_test_split(X_norm, Y_ohe, test_size=(1 / len(Y_ohe)), random_state=42)  # split sets & leave one out
 print('X_train_ohe:', X_train_ohe.shape, ' Y_train_ohe:', Y_train_ohe.shape)
--- ownneighbours.py
+++ ownneighbours.py
@@ -3,20 +3,23 @@ import operator
 def manhattan_distance(x, y):
 	return sum(abs(a - b) for a, b in zip(x, y))
 
-def getNeighbors(trainingSet, testInstance, k):
+def getNeighbors(trainingSet, testInstance, k): # returns list of index
 	distances = []
 	for x in range(len(trainingSet)):
 		dist = manhattan_distance(trainingSet[x], testInstance)
-		distances.append((trainingSet[x], dist))
+		distances.append((trainingSet[x], dist, x))
 	distances.sort(key=operator.itemgetter(1))
 	neighbors = []
 	for x in range(k):
-		neighbors.append(distances[x][0])
+		neighbors.append(distances[x][2])
 	return neighbors
 
 # muss angepasst werden
-def predict(trainingSet, testInstance, k):
-	neighbors = getNeighbors(trainingSet, testInstance, k)
-	output_values = [row[0] for row in neighbors]
-	prediction = max(set(output_values), key=output_values.count)
+def predict(X_train, X_test, Y_train, k):
+	neighbors = getNeighbors(X_train, X_test, k)
+	ouput = []
+	for row in neighbors:
+		index = neighbors[row]
+		ouput.append(Y_train[index])
+	prediction = max(set(output), key=output.count)
 	return prediction
