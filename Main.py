c4675ef50f82e550e94246f015ae8dd5813ece12
Henning <henning.richter@uni-rostock.de>
2020-05-24 16:13:34 +0200

    also for ohe

--- Main.py
+++ Main.py
@@ -12,7 +12,8 @@ X_norm = normalize(X)  # normalizing attributes
 # naive is already given
 Y_ohe = pd.get_dummies(Y)  # One Hot Encoding
 
-# hyperparameter tuning
+# hyperparameter
+
 k_range  = (1,(len(Y)-1))
 distance_metrics = ['euclidean','manhattan','chebyshev','minkowski']
 grid_params_knn= {
@@ -20,29 +21,44 @@ grid_params_knn= {
         'weights': ['uniform','distance'],
         'metric': distance_metrics}
 
-gs_knn = GridSearchCV(KNeighborsClassifier(), grid_params_knn, verbose = 1, cv = LeaveOneOut(), n_jobs = -1, scoring ="f1_micro")
-gs_knn_results = gs_knn.fit(X_norm, Y)
-parameters_knn = gs_knn.best_params_
-print(parameters_knn)
-
 kernel_functions = ['gaussian','tophat','epanechnikov','exponential','linear','cosine']
 kernel_bandwidths = 10 ** np.linspace(-1, 1, 100)
 grid_params_kernel= {
         'kernel': kernel_functions,
         'bandwidth': kernel_bandwidths}
 
-gs_kernel = GridSearchCV(KernelDensity(), {'kernel': kernel_functions, 'bandwidth': kernel_bandwidths}, cv = LeaveOneOut())
+
+# Naive
+
+# hyperparameter tuning
+gs_knn = GridSearchCV(KNeighborsClassifier(), grid_params_knn, verbose = 1, cv = LeaveOneOut(), n_jobs = -1, scoring ="f1_micro")
+gs_knn_results = gs_knn.fit(X_norm, Y)
+parameters_knn = gs_knn.best_params_
+print(parameters_knn)
+
+gs_kernel = GridSearchCV(KernelDensity(), grid_params_kernel, cv = LeaveOneOut())
 gs_kernel.fit(X_norm, Y)
 parameters_kernel = gs_kernel.best_params_
 print(parameters_kernel)
 
-# Naive
 X_train, X_test, Y_train, Y_test = train_test_split(X_norm, Y, test_size=(1 / len(Y)), random_state=42)  # split sets & leave one out
 print('X_train:', X_train.shape, ' Y_train:', Y_train.shape)
 print('X_test:', X_test.shape, ' Y_test:', Y_test.shape)
 
+
 # One Hot Encoding
+
+# hyperparameter tuning
+gs_knn = GridSearchCV(KNeighborsClassifier(), grid_params_knn, verbose = 1, cv = LeaveOneOut(), n_jobs = -1, scoring ="f1_micro")
+gs_knn_results = gs_knn.fit(X_norm, Y_ohe)
+parameters_knn = gs_knn.best_params_
+print(parameters_knn)
+
+gs_kernel = GridSearchCV(KernelDensity(), grid_params_kernel, cv = LeaveOneOut())
+gs_kernel.fit(X_norm, Y_ohe)
+parameters_kernel = gs_kernel.best_params_
+print(parameters_kernel)
+
 X_train_ohe, X_test_ohe, Y_train_ohe, Y_test_ohe = train_test_split(X_norm, Y_ohe, test_size=(1 / len(Y_ohe)), random_state=42)  # split sets & leave one out
 print('X_train_ohe:', X_train_ohe.shape, ' Y_train_ohe:', Y_train_ohe.shape)
 print('X_test_ohe:', X_test_ohe.shape, ' Y_test_ohe:', Y_test_ohe.shape)
-# continue here ...
